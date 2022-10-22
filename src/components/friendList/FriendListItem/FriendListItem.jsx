aea407961649ce4a6ba67c4afdf67865595d0065
Oleksii26 <Kolesnik26@ukr.net>
2022-10-22 22:38:28 +0300

    saveeee

--- src/components/friendList/FriendListItem/FriendListItem.jsx
+++ src/components/friendList/FriendListItem/FriendListItem.jsx
@@ -4,10 +4,10 @@ import css from '../friendList.module.css'
 export const FriendListItem = ({ avatar, name, isOnline, id }) => {
 
     return <li className={css.item} >
-        <span className={css.status}></span>
+        <span className={css.status} ></span>
         <div className={isOnline ? css.true : css.false}></div>
         <img className={avatar} src={avatar} alt={name} width="48" />
-        <p className='name'>{name}</p>
+        <p className={css.name}>{name}</p>
     </li>
 
 }
